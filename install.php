d66a5beae8a3bb81939b852c4c98734920fbe20a
alexplusde <github.com@alexplus.de>
2020-02-12 10:18:09 +0100

    MySQL5-Kompatibilität herstellen

--- install.php
+++ install.php
@@ -3,7 +3,7 @@
 rex_sql_table::get(rex::getTable('tmp_yform_spam_protection_frequency'))
     ->ensureColumn(new rex_sql_column('ipv4', 'int(10) unsigned', true))
     ->ensureColumn(new rex_sql_column('ipv6', 'varbinary(16)', true))
-    ->ensureColumn(new rex_sql_column('createdate', 'datetime', false, null, 'on update CURRENT_TIMESTAMP'))
+    ->ensureColumn(new rex_sql_column('createdate', 'datetime', false))
     ->ensureColumn(new rex_sql_column('was_blocked', 'bit(1)'))
     ->ensure();
 
