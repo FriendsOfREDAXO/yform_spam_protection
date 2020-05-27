533339224a209a6d54c329b717d87e73cab0e0d7
mg-gundb <33822420+mg-gundb@users.noreply.github.com>
2020-05-27 14:32:40 +0200

    Update value.spam_protection.tpl.php

--- ytemplates/bootstrap/value.spam_protection.tpl.php
+++ ytemplates/bootstrap/value.spam_protection.tpl.php
@@ -17,8 +17,8 @@
             opacity: 1;
         }
         [id="<?=$this->getHTMLId() ?>"] input {
-            background-color: transparent !important; 
-			border-color:transparent !important;
+		background-color: transparent !important; 
+		border-color:transparent !important;
         }
     </style>
     <script type="text/javascript">
