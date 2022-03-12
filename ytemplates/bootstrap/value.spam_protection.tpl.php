0670f175b7993bbbdf7e5ee6024aa54edcd9ccdd
Alexander Walther <aw@alexplus.de>
2022-03-12 19:17:09 +0100

    initial commit 2.0

--- ytemplates/bootstrap/value.spam_protection.tpl.php
+++ ytemplates/bootstrap/value.spam_protection.tpl.php
@@ -7,25 +7,17 @@
         name="<?= $this->getFieldId() ?>_microtime" type="hidden"
         value="<?= microtime(true) ?>" readonly="readonly"
         tabindex="-1">
-    <input id="<?= $this->getFieldId() ?>_js_enabled"
-        name="<?= $this->getFieldId() ?>_js_enabled" type="hidden"
-        value="0" readonly="readonly" tabindex="-1">
     <style>
         [id="<?= $this->getHTMLId() ?>"] {
- 		position: absolute !important;
-		width: 1px !important;
-		height: 1px !important;
-		padding: 0 !important;
-		margin: -1px !important;
-		overflow: hidden !important;
-		clip: rect(0, 0, 0, 0) !important;
-		white-space: nowrap !important;
-		border: 0 !important;
-	    }
+            position: absolute !important;
+            width: 1px !important;
+            height: 1px !important;
+            padding: 0 !important;
+            margin: -1px !important;
+            overflow: hidden !important;
+            clip: rect(0, 0, 0, 0) !important;
+            white-space: nowrap !important;
+            border: 0 !important;
+        }
     </style>
-    <script type="text/javascript">
-        var date = new Date();
-        document.getElementById("<?= $this->getFieldId() ?>_js_enabled").value =
-            date.getFullYear();
-    </script>
 </div>
